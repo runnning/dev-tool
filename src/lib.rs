d0f7d70d8ee91dccc2234e4ea0382dc5c27deec8
ywn <runningerror@qq.com>
2025-04-03 13:39:30 +0800

    删除多余引用

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,7 @@
-pub mod ui;
 pub mod logic;
-mod utils;
+pub mod ui;
+pub mod utils;
 
 slint::slint! {
     export { MainWindow } from "src/ui/main.slint";
 }
-
-pub use logic::time::TimeLogic;
-pub use logic::event::EventHandler;
-pub use logic::json::JsonLogic;
-pub use utils::time;
-pub use utils::json; 
--- src/logic/mod.rs
+++ src/logic/mod.rs
@@ -1,7 +1,3 @@
-pub mod time;
 pub mod event;
 pub mod json;
-
-pub use time::TimeLogic;
-pub use event::EventHandler;
-pub use json::JsonLogic; 
+pub mod time;
